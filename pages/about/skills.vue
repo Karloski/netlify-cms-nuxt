645e2a292e9bec904b705ee5cb4f4d0f92b1c43f
Karl Brown <karl@dessol.com>
2020-03-16 17:12:01 +0000

    Added categories icon and menu

--- pages/about/skills.vue
+++ pages/about/skills.vue
@@ -1,8 +1,54 @@
 <template>
-  <div class="flex flex-col flex-auto -my-4">
-    <h1 class="text-center py-4">
-      Skills
-    </h1>
+  <div class="flex flex-col flex-auto -my-4 relative">
+    <div class="relative text-center py-4">
+      <h1>
+        Skills
+      </h1>
+      <div class="absolute categories flex flex-col items-end right-0 top-0">
+        <div class="categories-icon flex flex-col items-center justify-center p-4">
+          <div class="categories-segment pb-2">
+            <div class="categories-segment-left">
+              <div />
+              <div />
+            </div>
+            <div class="categories-segment-right">
+              <div />
+              <div />
+            </div>
+          </div>
+          <div class="categories-segment py-2">
+            <div class="categories-segment-left">
+              <div />
+              <div />
+            </div>
+            <div class="categories-segment-right">
+              <div />
+              <div />
+            </div>
+          </div>
+          <div class="categories-segment py-2">
+            <div class="categories-segment-left">
+              <div />
+              <div />
+            </div>
+            <div class="categories-segment-right">
+              <div />
+              <div />
+            </div>
+          </div>
+        </div>
+        <div class="categories-menu p-4">
+          <div v-for="page in aboutPages" :key="page" class="flex space-between items-center -mx-2 py-2 cursor-pointer">
+            <span class="px-2">
+              <img src="/img/php.svg" alt="" class="icon">
+            </span>
+            <h4 class="px-4 capitalize">
+              {{ page }}
+            </h4>
+          </div>
+        </div>
+      </div>
+    </div>
     <div class="flex flex-auto -mx-4">
       <div class="flex flex-col flex-auto -my-2">
         <div class="flex justify-center items-center m-auto w-full max-w-lg py-2 relative">
@@ -47,8 +93,13 @@
 <script>
 export default {
   layout: 'projection',
-  asyncData ({ context }) {
-    return require('@/assets/content/pages/about/skills.json')
+  async asyncData ({ context }) {
+    const data = await require('@/assets/content/pages/about/skills.json')
+
+    return {
+      ...data,
+      aboutPages: await require.context('@/assets/content/pages/about/', false, /\.json$/).keys().map(p => /\.\/(.*)\.json/.exec(p)[1])
+    }
   },
   data () {
     return {
@@ -80,3 +131,84 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.categories {
+  &-active {
+    background: #181025;
+
+    .categories-icon {
+      border: 1px solid;
+      border-bottom-color: #181025;
+      border-radius: 1rem;
+      border-bottom-left-radius: 0;
+      border-bottom-right-radius: 0;
+      z-index: 1;
+    }
+
+    .categories-menu {
+      position: relative;
+      top: -1px;
+      border: 1px solid;
+      border-radius: 1rem;
+      border-top-right-radius: 0;
+    }
+  }
+
+  &-segment {
+    display: flex;
+    justify-content: space-between;
+    width: 50px;
+
+    &-left {
+      position: relative;
+      width: 10px;
+
+      :first-child {
+        width: 10px;
+        height: 10px;
+        border: 2px solid;
+        z-index: 1;
+        position: absolute;
+      }
+
+      :last-child {
+        width: 10px;
+        height: 10px;
+        border: 2px solid;
+        color: #8e1014;
+        position: absolute;
+        top: 0.5rem;
+        left: 0.5rem;
+        z-index: 0;
+        filter: blur(2px);
+      }
+    }
+
+    &-right {
+      position: relative;
+      width: 35px;
+
+      :first-child {
+        width: 35px;
+        height: 10px;
+        border: 2px solid;
+        z-index: 1;
+        position: absolute;
+      }
+
+      :last-child {
+        width: 35px;
+        height: 10px;
+        border: 2px solid;
+        color: #8e1014;
+        position: absolute;
+        top: 0.5rem;
+        left: 0.5rem;
+        z-index: 0;
+        filter: blur(2px);
+      }
+    }
+  }
+}
+</style>
