4f82d1b971a00fb2c3dd40cf3e9016ecca2ce311
carlwithak <me@carlwithak.me.uk>
2022-12-28 19:48:05 +0000

    Updated projects page to add some additional information at a glance

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -6,7 +6,15 @@
     <div class="projects-list flex flex-auto overflow-auto justify-center items-start py-4">
       <transition-group v-if="projects.length > 0" name="gallery" tag="div" class="projects-item flex flex-wrap flex-auto w-full justify-center content-evenly">
         <div v-for="project in projects" :key="project.title" class="relative overflow-hidden w-full max-w-xl m-4 transition-all duration-500 bg-cover bg-center rounded-lg" :style="`background-image: url(${project.carouselItems[0].images[0].source})`">
-          <div class="projects-item-mask flex items-center justify-center h-full w-full absolute transform translate-y-full">
+          <div class="projects-item-mask flex flex-col gap-4 items-center justify-center h-full w-full absolute transform translate-y-full">
+            <span class="text-xl font-bold">
+              {{ project.title }}
+            </span>
+
+            <span>
+              {{ excerpt(project.about) }}
+            </span>
+
             <nuxt-link :to="`/projects/${project.slug}`" class="px-6 py-4 bg-transparent border rounded-lg button">
               View Project
             </nuxt-link>
@@ -18,6 +26,8 @@
 </template>
 
 <script>
+import { createExcerpt } from '~/common/util'
+
 export default {
   transition: {
     name: 'slide',
@@ -33,6 +43,14 @@ export default {
     projects () {
       return this.$store.state.projects
     }
+  },
+  methods: {
+    excerpt (text) {
+      return createExcerpt({
+        text,
+        length: 50
+      })
+    },
   }
 }
 </script>
